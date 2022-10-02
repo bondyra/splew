cd3dae308cc038fdba2b044cde60ad937cdf6de0
root <root@swiniak.localdomain>
2022-10-03 01:24:33 +0200

    Init

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const rules = require('require.all')('./tasks/rules');
+const plugins = require('require.all')('./tasks/plugins');
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = (options) => {
+  const environment = options.WEBPACK_BUNDLE ? 'production' : 'development';
+
+  rules((name, rule) => rule(environment));
+  plugins((name, rule) => rule(environment));
+
+  return {
+    mode: environment,
+    entry: {
+      app: path.resolve(__dirname, 'src/app/app.ts')
+    },
+    output: {
+      filename: '[name].js'
+    },
+    module: {
+      rules: [...rules.files, ...rules.scripts]
+    },
+    plugins: [plugins.html, plugins.images],
+    devServer: {
+      open: true,
+      port: 4000,
+      https: false,
+      hot: true,
+      historyApiFallback: true,
+      watchOptions: {
+        poll: true
+      }
+      // proxy: { '/api': 'http://localhost:3000' }
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin()],
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: 'all',
+            test: path.resolve(__dirname, 'node_modules'),
+            name: 'vendor',
+            enforce: true
+          }
+        }
+      }
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+      alias: {
+        styles: path.resolve(__dirname, 'src/styles'),
+        assets: path.resolve(__dirname, 'src/assets'),
+        '~': path.resolve(__dirname, 'src/app')
+      }
+    }
+  };
+};
